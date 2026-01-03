748b227581085770802570ac862094b80174a506
08shristi <shristipatwa8@gmail.com>
2026-01-03 19:55:11 +0530

    Add files via upload

---
+++ 06_input.py
@@ -0,0 +1,12 @@
+a = input("Enter the number 1:")
+b = input("Enter the number 2:")
+
+print("Number a is:", a)
+print("Number b is:", b)
+print("sum is:", a + b)# they will give a string output which is 12 because we cann't declare int type conversion
+
+a = int(input("Enter a number 1:"))
+b = int(input("Enter a number b:"))
+print("sum is:", a+b)
+
+# we cnn't type conversion the string on;y because it was default
