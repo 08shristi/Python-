8c23e43ef2f9ff829f8842ef1ee6c0a536724b8a
08shristi <shristipatwa8@gmail.com>
2026-01-03 19:54:36 +0530

    Add files via upload

---
+++ 05_type.py
@@ -0,0 +1,26 @@
+a = 31
+t = type(a)
+print(t)
+# type function ka use karka kese v type ka variable ko declare kar sakta ha
+b = "shristi"
+t = type(b)
+print(t)
+
+c = 7.5
+t = type(c)
+print(t)
+
+d = "7"# string dega
+t = type(d)
+print(t)
+# Type function ka use karka type change kar sakta ha
+
+e = "54.4"# it change string into float....
+f = float(e)
+t = type(f)
+print(t)
+
+g = "32"# it convert string to integer.....
+h = int(g)
+t = type(h)
+print(t)
