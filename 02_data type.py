6ee4dd914ac62696d91cdde006a2d68dad7ef49b
08shristi <shristipatwa8@gmail.com>
2026-01-03 19:53:29 +0530

    Add files via upload

---
+++ 02_data type.py
@@ -0,0 +1,9 @@
+a = 1 # a is an integer...
+
+b = 5.52 #b is an floating point number
+
+c = "shristi"# c is an string....
+
+d = True , False # d is an bollen vairable
+
+e = None # e is a none type variable
