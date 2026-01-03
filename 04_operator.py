ec0a77a34f28e712b6f4518836086073d61cf70f
08shristi <shristipatwa8@gmail.com>
2026-01-03 19:54:18 +0530

    Add files via upload

---
+++ 04_operator.py
@@ -0,0 +1,39 @@
+#Arthimatic operator
+a = 7
+b = 4
+c = a + b
+print(c)
+
+
+#assignment operator
+a = 4-2# assign means = thats means assign 4- 2 into a
+print(a)
+b = 6
+#b += 3 # it will increment the value of b with 3 thats means 6+3= 9
+b -= 3 # it will decrement the value of b with 3 thats means 6-3= 3
+print(b)
+
+#Comparison operator
+
+d = 5>4# it alway return a boolen thats means true or false
+print (d)
+
+#logical operator
+
+e = True or False# or ka liya dono ma koi ek jij true hogi to bo true return karaga
+
+#Truth table of "or"
+print("True or False is ", True or False) 
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of "and"
+print("True and False is ", True and False) 
+print("True and True is ", True  and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+# Truth table of "Not"
+
+print(not(True))
